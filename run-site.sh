7be429823dad6c03eb7f356029221703565cf367
Jan Sarenik <jajomojo@gmail.com>
2020-01-14 21:38:40 +0100

    Add example Caddy configuration generator to run-site.sh

--- run-site.sh
+++ run-site.sh
@@ -2,4 +2,14 @@
 
 PORT=${1:-"8890"}
 echo "Starting httpd at http://localhost:$PORT"
+echo
+echo "Example Caddy configuration:"
+cat <<EOF
+clock.mysite.eu.org {
+  proxy / http://127.0.0.1:$PORT {
+    transparent
+  }
+  gzip
+}
+EOF
 httpd -c httpd.conf -f -p $PORT
