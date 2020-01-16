3ebe28980bc505f323476d6fcbe3f9610116a696
Jan Sarenik <jajomojo@gmail.com>
2020-01-16 19:25:52 +0100

    timeapi.php: Use echo instead of cat

--- css/timeapi.php
+++ css/timeapi.php
@@ -4,7 +4,4 @@ echo "Content-Type: text/html; charset=UTF-8"
 echo
 
 T=$(/busybox/timestamp | /busybox/sed -E 's/(...)$/\.\1/')
-
-/busybox/cat <<EOF
-var server = $T; var dtest = new Date();
-EOF
+echo "var server = $T; var dtest = new Date();"
