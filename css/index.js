a30384dfa8bf896327b82039fb4190703a8e1844
Jan Sarenik <jajomojo@gmail.com>
2020-02-10 15:17:57 +0100

    index.js: Simplify and get rid of decisecond magic

--- css/index.js
+++ css/index.js
@@ -1,18 +1,10 @@
 var today = new Date();
-var client = parseInt(today.getTime());
-var offsetfix = parseInt((client-server)/10); <!-- deciseconds -->
-
-var h = today.getHours();
-var mi = today.getMinutes();
-var s = today.getSeconds();
-var ds = today.getMilliseconds();
-ds = parseInt(ds/10);
-var timeSeconds = (h*360000+mi*6000+s*100+ds); //h*3600+m*60+s
-var startmoment = today;
-var totalSeconds = timeSeconds-offsetfix;
+var client = today.getTime();
+var offsetfix = client-server;
+var timeSeconds = today.getTime();
 var da = 0;
 var ne = 0;
-var starttime = 0;
+var syncstart = 0;
 
 function showds() {
 	document.getElementById('decs').style.visibility='visible';
@@ -23,25 +15,25 @@ function hideds() {
 function dosync() {
   $.ajax({url: "/css/timeapi2.php",cache:false, timeout:1800,
   beforeSend: function(xhr) {
-	starttime = parseInt(new Date().getTime());
+	syncstart = new Date().getTime();
   },
   error: function(xhr){
 	ne++;
 	document.getElementById('fal').innerHTML = ne;
-	nextsync = 3000 + 2*ne;
+	nextsync = 3000 + ne*2000;
 	synctimer = setTimeout(dosync, nextsync);
   },
   success: function(result){
 	today = new Date();
-	client = parseInt(today.getTime());
-	offsetfix = parseInt((starttime-parseInt(result))/100);
+	client = today.getTime();
+	offsetfix = syncstart-result;
 	da++;
 	document.getElementById('suc').innerHTML = da;
-	precision = (client - starttime)/1000;
+	precision = (client - syncstart)/1000;
 	document.getElementById('pre').innerHTML = precision;
 	if(precision >1) document.getElementById('pre').style.color='RED'; else document.getElementById('pre').style.color='GREEN';
-	document.getElementById('ofs').innerHTML = offsetfix/100;
-	document.getElementById('lte').innerHTML = pad(parseInt(totalSeconds/360000)%24) + ':' + pad(parseInt(totalSeconds/6000)%60) + ':' + pad(parseInt(totalSeconds/100)%60);
+	document.getElementById('ofs').innerHTML = offsetfix;
+	document.getElementById('lte').innerHTML = hms;
 	nextsync = 3000;
 	if(da>5) nextsync = 10000;
 	if(da>10) nextsync = 30000;
@@ -51,15 +43,18 @@ function dosync() {
   }});
 } // end of dosync()
 function doTime() {
-  var now = new Date();
-  totalSeconds = parseInt((now - startmoment)/10+timeSeconds-offsetfix);
-  totalSeconds = (totalSeconds+8640000)%8640000;
+  var now = new Date().getTime();
+  totalSeconds = now-offsetfix;
 
-  decsLabel.innerHTML = '.'+(parseInt(totalSeconds/10)%10);
-  secondsLabel.innerHTML = pad(parseInt(totalSeconds/100)%60);
-  minutesLabel.innerHTML = pad(parseInt(totalSeconds/6000)%60);
-  hoursLabel.innerHTML = pad(parseInt(totalSeconds/360000)%24);
-  document.title = hoursLabel.innerHTML + ":" + minutesLabel.innerHTML + ":" + secondsLabel.innerHTML;
+  decsLabel.innerHTML = '.'+(totalSeconds/10)%10;
+  s = pad(totalSeconds/100)%60;
+  m = pad(totalSeconds/6000)%60;
+  h = pad(totalSeconds/360000)%24;
+  hms = h + ':' + m + ':' + s;
+  document.title = hms;
+  secondsLabel.innerHTML = s;
+  minutesLabel.innerHTML = m;
+  hoursLabel.innerHTML = h;
 }
 function pad(val) {
   var str = "0" + val;
