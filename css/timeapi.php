36bc0ea759375ffcbacf3a95f4dd51a16500e4af
Jan Sarenik <jajomojo@gmail.com>
2020-02-11 13:56:04 +0100

    timeapi.php: Return in one var separated by comma

--- css/timeapi.php
+++ css/timeapi.php
@@ -4,4 +4,4 @@ echo "Content-Type: text/javascript"
 echo
 
 T=$(/busybox/timestamp | /busybox/sed -E 's/(...)$/\.\1/')
-echo "var server = $T; var dtest = new Date();"
+echo "var dtest=new Date(), server=$T;"
