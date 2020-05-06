fb4b50ad7a84dabc83f3719654a7d97feebfc224
Jan Sarenik <jajomojo@gmail.com>
2020-05-06 10:09:08 +0200

    Add convertpos.sh - WIP

---
+++ convertpos.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Example input +513030-0000731
+# (London)
+# Example output "left:49.97%; top:21.38%;"
+
+#expecttest "+513030-0000731" "left:49.97%; top:21.38%;"
+
+convertpos() {
+  :
+}
